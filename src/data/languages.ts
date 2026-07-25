c4c8b6a6324d1d4ddd182a2122759ab24d25b0d2
Farseen <farsukoothradan@gmail.com>
2026-07-25 22:31:18 +0530

    lang content added

---
+++ src/data/languages.ts
@@ -0,0 +1,25 @@
+import type { Language } from "@/types/learning";
+
+export const languages: Language[] = [
+  {
+    code: "es",
+    name: "Spanish",
+    nativeName: "Español",
+    flag: "https://flagcdn.com/w320/es.png",
+    color: "#FF9600",
+  },
+  {
+    code: "fr",
+    name: "French",
+    nativeName: "Français",
+    flag: "https://flagcdn.com/w320/fr.png",
+    color: "#4D8BFF",
+  },
+  {
+    code: "ja",
+    name: "Japanese",
+    nativeName: "日本語",
+    flag: "https://flagcdn.com/w320/ja.png",
+    color: "#FF4D4F",
+  },
+];
